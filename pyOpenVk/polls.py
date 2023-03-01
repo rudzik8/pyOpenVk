bd21a7b824fa831e8954ed78a2727575fec91e35
Mikita Wiśniewski <rudzik8@protonmail.com>
2023-03-01 18:05:11 +0700

    Implement polls API
    thx to celestora for some funny meat fixes <3

---
+++ pyOpenVk/polls.py
@@ -0,0 +1,18 @@
+import requests as http
+import json
+from pyOpenVk import api
+from pyOpenVk import wall
+
+class polls:
+
+    @staticmethod
+    def get_poll(client, poll_id, extended=0, fields=''):
+        return http.get(f'https://{client["instance"]}/method/Polls.getById?poll_id={poll_id}&extended={extended}&fields={fields}&access_token={client["token"]}').json()
+
+    @staticmethod
+    def add_vote(client, poll_id, answers_ids):
+        return http.get(f'https://{client["instance"]}/method/Polls.addVote?poll_id={poll_id}&answers_ids={answers_ids}&access_token={client["token"]}').json()
+
+    @staticmethod
+    def delete_vote(client, poll_id):
+        return http.get(f'https://{client["instance"]}/method/Polls.deleteVote?poll_id={poll_id}&access_token={client["token"]}').json()
